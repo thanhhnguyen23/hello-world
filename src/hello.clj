efe89326e8ea4e1abbbc084ad92c02feb33d7112
thanhhnguyen23 <thanh.h.nguyen23@gmail.com>
2023-12-26 19:23:51 -0500

    using local libraries

--- src/hello.clj
+++ src/hello.clj
@@ -1,15 +1,19 @@
-(ns hello (:require [java-time.api :as t]))
+;; using library from maven repo
+;;(ns hello (:require [java-time.api :as t]))
+
+;; using local libraries
+(ns hello (:require [hello-time :as ht]))
 
 ;; $ clj -X hello/run
-(defn time-str
-  "returns a string representation of a datetime in the local timezone."
-  [instant]
-  (t/format
-   (t/with-zone (t/formatter "hh:mm a") (t/zone-id))
-   instant))
+;;(defn time-str
+;;  "returns a string representation of a datetime in the local timezone."
+;;  [instant]
+;;  (t/format
+;;   (t/with-zone (t/formatter "hh:mm a") (t/zone-id))
+;;   instant))
 
 (defn run [opts]
-  (println "hello world, the time is" (time-str (t/instant))))
+  (println "hello world, the time is" (ht/time-str (ht/now))))
 
 (comment
 )
